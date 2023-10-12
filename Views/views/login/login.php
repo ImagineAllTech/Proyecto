07d3845738bc5e2a7fbab25c93db695ae9071aa0
Matias Moreira <matiasmoreira212@gmail.com>
2023-10-12 14:44:09 -0300

    New login ui

--- Views/views/login/login.php
+++ Views/views/login/login.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
-<html id="html" class="" lang="en">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CUK | Log In</title>
-    <link rel="shortcut icon" href="assets/img/Logotype.svg" type="image/x-icon">
-    <link rel="stylesheet" href="../../assets/css/jueces.css">
+    <title>CUK | Inicio de sesion</title>
+    <link rel="shortcut icon" href="../../assets/img/Logotype.svg" type="image/x-icon">
 
-
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://kit.fontawesome.com/cea33d77ef.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../assets/css/normalize.css">
+    <script src='https://cdn.tailwindcss.com'></script>
+    <script src='https://kit.fontawesome.com/cea33d77ef.js' crossorigin='anonymous'></script>
+    <style media='all' id='fa-v4-font-face'></style>
 </head>
+<body class="bg-blue-700 flex items-center justify-center h-screen w-full overflow-hidden">
+    
+    <main class="bg-gray-50 h-5/6 w-5/12 rounded-md flex items-center justify-between overflow-hidden">
 
-<body class="body flex flex-col justify-center items-center w-full min-h-screen font-montserrat">
+        <section class="w-full w-full h-full flex flex-col py-10 px-10 items-start justify-between">
+            <header class="flex items-center justify-end gap-4 w-full ">
+                <h5 class="font-poppins text-lg text-gray-600">Te equivocaste de boton?</h5>
+                <a href="../../../index.php" class="font-poppins text-gray-600 hover:shadow-md transition-all duration-300 font-black w-28 rounded-full h-10 border-2 grid place-content-center">VOLVER</a>
+            </header>
 
-    <a href="../../../index.php" class="fixed top-0 px-4 py-4">
-        <img src="../../assets/img/Logotype.svg" alt="Icono" class="w-20">
-    </a>
-
-    <main class="h-screen w-full flex items-center justify-center">
-        <form method="post" action="#"
-            class="w-10/12 lg:w-3/12 h-4/6 rounded-md flex flex-col items-center justify-around border border-gray-900">
-            <div class="form__container flex items-start justify-center flex-col gap-8 relative">
-                <h1 class="font-bricolage text-4xl text-blue-800 font-bold">Iniciar Sesion</h1>
-                <!-- <img src="../../assets/img/Logotype.svg" alt="Icono" class="w-8 absolute top-0 right-2"> -->
+            <form 
+            action="#"
+            method="post"
+            class="w-full"
+            >
+                <div class="w-full texts flex flex-col justify-start items-start mb-8">
+                    <h1 class=" font-tw text-5xl text-blue-900">Inicio de sesion</h1>
+                    <p class="hidden md:flex font-poppins text-lg text-gray-500">Ingresa en tu cuenta con usuario y contraseña</p>
+                </div>
 
                 <?php
                 include("../../../Models/config/database.php");
@@ -33,30 +37,39 @@
                 
                 ?>
 
-                <div class="form__group relative">
-                    <input type="text"
-                        class="w-full bg-transparent border-b outline-none py-2 px-4 text-gray-900 font-bricolage text-xl border-blue-800 origin-top-left translate-y-3"
-                        placeholder="Escriba aqui" name="username">
-                    <label for="username" class="absolute top-0 left-3 cursor-pointer px-1">Usuario</label>
+                <div class="form__container flex flex-col items-center justify-center mt-4 gap-6">
+
+                    <div class="form__group relative flex w-full flex-col items-start justify-center">
+                        <label for="username" class="mb-1 font-poppins text-lg text-gray-600">Usuario:</label>
+                        <input type="text" name="username" placeholder="Ingresa tu usuario"
+                        class="h-12 w-full rounded-md outline-none border focus:border-white focus:outline-blue-800 font-montserrat text-blue-900 px-4 font-semibold text-xl">
+                        <!-- <label for="username" class="cursor-pointer absolute top-10 right-3 text-2xl text-blue-900"><i class="fa-solid fa-circle-user"></i></label> -->
+                    </div>
+
+                    <div class="form__group relative flex w-full flex-col items-start justify-center">
+                        <label for="pass" class="mb-1 font-poppins text-lg text-gray-600">Contraseña:</label>
+                        <input type="password" id="passwordField" name="pass" placeholder="Ingresa tu usuario"
+                        class="h-12 w-full rounded-md outline-none border focus:border-white focus:outline-blue-800 font-montserrat text-blue-900 px-4 font-semibold text-xl">
+                        <!-- <label for="pass" class="cursor-pointer absolute top-10 right-3 text-2xl text-blue-900"><i class="fa-solid fa-eye"></i></label> -->
+                        <button type="button" id="togglePassword" class="absolute top-10 right-3 text-2xl text-blue-900"><i class="fa-solid fa-eye"></i></button>
+                
+                    </div>
+                    
+                    <div class="button">
+                        <input type="submit" name="ingresar" value="Ingresar" class="font-poppins w-48 h-14 bg-blue-600 rounded-full text-white text-xl cursor-pointer hover:bg-blue-700">
+                    </div>
                 </div>
+            </form>
 
-                <div class="form__group relative">
-                    <input type="password" placeholder="Escriba aqui" class="w-full bg-transparent border-b outline-none py-2 px-4 text-gray-900 font-bricolage text-xl border-blue-800 origin-top-left translate-y-3" name="pass" id="passwordField">
-                    <label for="pass" class="absolute top-0 left-3 cursor-pointer px-1">Contraseña</label>
-                    <button type="button" id="togglePassword" class="absolute top-6 right-5"><i class="fa-solid fa-eye"></i></button>
-                </div>
+            <footer class="flex items-center justify-start gap-4 w-full ">
+                <h5 class="font-poppins text-lg text-gray-600">Quieres contactarnos? <a href="#" class="text-blue-600 hover:underline">Clickea aqui</a></h5>
+            </footer>
 
+        </section>
 
-                <div class="form__button w-full">
-                    <input type="submit" value="Ingresar" name="ingresar"
-                        class="w-full cursor-pointer h-10 bg-blue-800 hover:bg-blue-900 text-xl font-montserrat text-white font-bold rounded-md">
-                </div>
-            </div>
-
-        </form>
     </main>
 
-    <script src="../../../Views/assets/js/login.js"></script>
-</body>
 
-</html>
+    <script src="../../assets/js/login.js"></script>
+</body>
+</html> 
