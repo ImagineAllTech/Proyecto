297e3a0f8f2acf5b47ad7ad4266770cab2cffa56
Matias Moreira <matiasmoreira212@gmail.com>
2023-10-17 00:40:13 -0300

    Nuevo enrutador global, este se desarollo ya que el otro se usaba dependiendo lo que se quiera crear

---
+++ Models/enrutador.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "config/config.php";
+require_once "core/routes.php";
+require_once "config/database.php";
+require_once "../Controllers/competidores.php";
+require_once "../Controllers/peticionComp.php";
+
+if(isset($_GET['c'])){
+		
+    $controlador = cargarControlador($_GET['c']);
+    
+    if(isset($_GET['a'])){
+        if(isset($_GET['ID'])){
+            cargarAccion($controlador, $_GET['a'], $_GET['ID']);
+            } else {
+            cargarAccion($controlador, $_GET['a']);
+        }
+
+        } else {
+        cargarAccion($controlador, ACCION_PRINCIPAL);
+    }
+    
+    } else {
+    
+    $controlador = cargarControlador(CONTROLADOR_PRINCIPAL);
+    $accionTmp = ACCION_PRINCIPAL;
+    $controlador->$accionTmp();
+}
+
+?>
